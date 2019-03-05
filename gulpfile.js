239618d7b5d8b321088bb07fa22718d4859bde8e
Chen Jian <cjian101@gmail.com>
2019-03-06 00:07:22 +0200

    Better gulpfile, scale images on hover

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,25 @@ const imageResize = require('gulp-image-resize');
 const replace = require('gulp-replace');
 const fs = require('fs');
 
-function downscale(cb) {
+function downscale() {
   const all = fs.readdirSync('images/raw');
   const downscaled = fs.readdirSync('images/downscaled');
   const diff = all.filter(filename => downscaled.indexOf(filename) < 0);
+
   if (!diff.length)
-    return cb();
-  src(diff, { cwd: 'images/raw' })
-  .pipe(imageResize({ percentage: 30 }))
-  .pipe(dest('images/downscaled/'))
-  .on('end', cb);
+    return Promise.resolve('There is nothing to be done');
+
+  return src(diff, { cwd: 'images/raw' })
+         .pipe(imageResize({ percentage: 30 }))
+         .pipe(dest('images/downscaled/'))
 }
 
 function count(cb) {
   const imgCount = fs.readdirSync('images/raw').length;
-  src('script.js')
-  .pipe(replace(/^const numImgs = \d+;/, `const numImgs = ${imgCount};`))
-  .pipe(dest('.'))
-  .on('end', cb);
+  return src('script.js')
+         .pipe(replace(/^const numImgs = \d+;/, `const numImgs = ${imgCount};`))
+         .pipe(dest('.'))
+         .on('end', cb);
 }
 
 function clean() {
