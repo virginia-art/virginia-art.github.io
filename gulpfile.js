332d19fda065a0555e8104e7ede007eb542bb38a
Chen Jian <cjian101@gmail.com>
2019-03-05 23:58:33 +0200

    Downscale to 30% and badass build script

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,24 @@ const imageResize = require('gulp-image-resize');
 const replace = require('gulp-replace');
 const fs = require('fs');
 
-function downscale() {
-  return src('images/raw/*.png')
-         .pipe(imageResize({ percentage: 50 }))
-         .pipe(dest('images/downscaled/'));
+function downscale(cb) {
+  const all = fs.readdirSync('images/raw');
+  const downscaled = fs.readdirSync('images/downscaled');
+  const diff = all.filter(filename => downscaled.indexOf(filename) < 0);
+  if (!diff.length)
+    return cb();
+  src(diff, { cwd: 'images/raw' })
+  .pipe(imageResize({ percentage: 30 }))
+  .pipe(dest('images/downscaled/'))
+  .on('end', cb);
 }
 
-async function count() {
+function count(cb) {
   const imgCount = fs.readdirSync('images/raw').length;
-  await src('script.js')
-        .pipe(replace(/^const numImgs = \d+;/, `const numImgs = ${imgCount};`))
-        .pipe(dest('.'));
+  src('script.js')
+  .pipe(replace(/^const numImgs = \d+;/, `const numImgs = ${imgCount};`))
+  .pipe(dest('.'))
+  .on('end', cb);
 }
 
 function clean() {
