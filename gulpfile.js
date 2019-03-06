eb4ded8e01b5b984a2383c15e1d1c8e9daf1dc79
Chen Jian <cjian101@gmail.com>
2019-03-06 13:28:49 +0200

    Fix bugs and include JQuery in repo

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,24 @@ const imageResize = require('gulp-image-resize');
 const replace = require('gulp-replace');
 const fs = require('fs');
 
-function downscale() {
+function downscale(cb) {
   const all = fs.readdirSync('images/raw');
   const downscaled = fs.readdirSync('images/downscaled');
-  const diff = all.filter(filename => downscaled.indexOf(filename) < 0);
+  const diff = all.filter(filename => filename != '.DS_Store' && downscaled.indexOf(filename) < 0);
 
   if (!diff.length)
     return Promise.resolve('There is nothing to be done');
 
   return src(diff, { cwd: 'images/raw' })
          .pipe(imageResize({ percentage: 30 }))
-         .pipe(dest('images/downscaled/'))
+         .pipe(dest('images/downscaled/'));
 }
 
 function count(cb) {
-  const imgCount = fs.readdirSync('images/raw').length;
+  const imgCount = fs.readdirSync('images/raw').filter(file => file != '.DS_Store').length;
   return src('script.js')
          .pipe(replace(/^const numImgs = \d+;/, `const numImgs = ${imgCount};`))
-         .pipe(dest('.'))
-         .on('end', cb);
+         .pipe(dest('.'));
 }
 
 function clean() {
