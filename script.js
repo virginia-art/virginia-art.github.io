b89567f98e1f3f0185d29041edd0b1d55652cd54
Chen Jian <cjian101@gmail.com>
2019-03-07 00:17:14 +0200

    Add loading screen

--- script.js
+++ script.js
@@ -2,29 +2,35 @@ const numImgs = 12; // This line will be changed by gulp
 
 const gallery = $('#gallery');
 const previewContainer = $('#img-preview');
+const loading = $('#loading');
 
 for (let i = 0; i < numImgs; i++) {
-    const img = $(`<img id="img-${i}"
-                        class="gallery-img"
-                        src="images/downscaled/${i}.png"
-                        width="${gallery.width() / 4}" />`);
-    gallery.append(img);
+  const img = $(`<img id="img-${i}"
+                      class="gallery-img"
+                      src="images/downscaled/${i}.png"
+                      width="${gallery.width() / 4}" />`);
+  gallery.append(img);
 }
 
 $(window).on('resize', () => {
-    for (let i = 0; i < numImgs; i++) {
-        $(`#gallery img#img-${i}`).attr('width', gallery.width() / 4);
-    }
+  for (let i = 0; i < numImgs; i++) {
+    $(`#gallery img#img-${i}`).attr('width', gallery.width() / 4);
+  }
 });
 
 $('.gallery-img').on('click', e => {
-    const imgNum = $(e.target).attr('id').split('-')[1];
-    const preview = $(`<img src="images/raw/${imgNum}.png" />`);
-    previewContainer.append(preview);
-    previewContainer.show();
+  const imgNum = $(e.target).attr('id').split('-')[1];
+  const preview = $(`<img src="images/raw/${imgNum}.png" />`);
+  previewContainer.append(preview);
+  previewContainer.show();
 })
 
 $(previewContainer).on('click', () => {
-    previewContainer.hide();
-    previewContainer.empty();
+  previewContainer.hide();
+  previewContainer.empty();
+});
+
+$(window).on('load', () => {
+    loading.addClass('hidden');
+    setTimeout(() => loading.hide(), 500);
 });
